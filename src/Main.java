51025f2906c59097ea357a721a4b65480859d8c8
SSD-11 <samuserna2005@gmail.com>
2022-02-26 15:36:52 -0500

    Algoritmo de practica de Constructores con java POO

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        Auto ferrari = new Auto("Ferrari", "296 GTB 2021", "Amarillo");
+        Auto ferrari2 = new Auto(800);
+
+
+        //Metodos
+        System.out.println("");
+        System.out.println("Los Metodos del Vehiculo son:");
+        System.out.println("");
+        ferrari.arrancar();
+        System.out.println(ferrari.acelerar(100));
+        System.out.println("-El vehiculo esta girando a la:" +ferrari.girar(1));
+        ferrari.frenar();
+        ferrari.reversar();
+    }
+
+
+}
